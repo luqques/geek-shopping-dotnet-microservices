97b6727885d8ee49a24365e40136a739487fdfe5
Lucas da Cunha <luques.cunha@gmail.com>
2024-05-27 20:19:52 -0300

    Criando classe de Produto

--- GeekShopping.ProductsApi/Model/Context/MySqlContext.cs
+++ GeekShopping.ProductsApi/Model/Context/MySqlContext.cs
@@ -7,6 +7,6 @@ namespace GeekShopping.ProductsApi.Model.Context
         public MySqlContext() {}
         public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) {}
                
-
+        public DbSet<Product> Products { get; set; }
     }
 }
