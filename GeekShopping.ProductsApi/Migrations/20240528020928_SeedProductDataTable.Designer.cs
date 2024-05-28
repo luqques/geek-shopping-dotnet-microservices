9e042ce4526599a1768b2ea6bf43c43692be1c17
Lucas da Cunha <luques.cunha@gmail.com>
2024-05-27 23:16:23 -0300

    Executando as Seeds (Migrations) e populando o banco de dados

--- GeekShopping.ProductsApi/Migrations/20240528020928_SeedProductDataTable.Designer.cs
+++ GeekShopping.ProductsApi/Migrations/20240528020928_SeedProductDataTable.Designer.cs
@@ -3,6 +3,7 @@ using GeekShopping.ProductsApi.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.ProductsApi.Migrations
 {
     [DbContext(typeof(MySqlContext))]
-    partial class MySqlContextModelSnapshot : ModelSnapshot
+    [Migration("20240528020928_SeedProductDataTable")]
+    partial class SeedProductDataTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
