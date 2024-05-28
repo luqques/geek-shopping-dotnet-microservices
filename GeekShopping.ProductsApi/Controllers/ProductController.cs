0be3a0d0741519901f538a22f489d2f878038b44
Lucas da Cunha <luques.cunha@gmail.com>
2024-05-27 22:44:09 -0300

    Adicionado app.MapControllers no Program.cs e criado Controller de produtos

---
+++ GeekShopping.ProductsApi/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using GeekShopping.ProductsApi.Data.ValueObjects;
+using GeekShopping.ProductsApi.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductsApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> FindAll()
+        {
+            var products = await _repository.FindAll();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDto>> FindById(long id)
+        {
+            var product = await _repository.FindById(id);
+            if (product is null) 
+                return NotFound();
+
+            return Ok(product);
+        }
+    }
+}
--- GeekShopping.ProductsApi/Program.cs
+++ GeekShopping.ProductsApi/Program.cs
@@ -13,7 +13,7 @@ var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
 builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(
     connection,
-    new MySqlServerVersion(new Version(8, 0, 29)))
+    new MySqlServerVersion(new Version(8, 0, 2)))
 );
 
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
@@ -29,6 +29,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "GeekShopping", Version =  "v1"});
+    c.EnableAnnotations();
 });
 
 var app = builder.Build();
@@ -40,29 +41,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
