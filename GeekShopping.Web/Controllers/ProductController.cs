a3e76f161e1dbc293324a9a02850c6db7e0a6a42
Lucas da Cunha <luques.cunha@gmail.com>
2024-05-28 23:51:34 -0300

    Criado controller e view para ProductDelete

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -52,9 +52,32 @@ namespace GeekShopping.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var response = await _productService.CreateProduct(model);
+                var response = await _productService.UpdateProduct(model);
 
                 if (response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null)
+                return View(product);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.DeleteProductById(model.Id);
+
+                if (response)
                     return RedirectToAction(nameof(ProductIndex));
             }
 
