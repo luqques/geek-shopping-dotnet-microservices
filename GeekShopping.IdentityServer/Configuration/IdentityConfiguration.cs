5f9ae123b285a51d5824bb841db53dd7537953e5
Lucas da Cunha <luques.cunha@gmail.com>
2024-05-29 23:12:33 -0300

    Alterando para padronizar as portas utilizadas e usar HTTPS

--- GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -7,7 +7,7 @@ namespace GeekShopping.IdentityServer.Configuration
     public static class IdentityConfiguration
     {
         public const string Admin = "Admin";
-        public const string Customer = "Customer";
+        public const string Client = "Client";
 
         public static IEnumerable<IdentityResource> IdentityResources =>
             new List<IdentityResource>
@@ -41,8 +41,8 @@ namespace GeekShopping.IdentityServer.Configuration
                     ClientId = "geek_shopping",
                     ClientSecrets = { new Secret("my_super_secret".ToSha256())},
                     AllowedGrantTypes = GrantTypes.Code,
-                    RedirectUris = {"http://localhost:26357/signin-oidc"},
-                    PostLogoutRedirectUris = {"http://localhost:26357/signout-callback-oidc"},
+                    RedirectUris = {"https://localhost:4430/signin-oidc"},
+                    PostLogoutRedirectUris = {"https://localhost:4430/signout-callback-oidc"},
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
--- GeekShopping.ProductsApi/Program.cs
+++ GeekShopping.ProductsApi/Program.cs
@@ -47,6 +47,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+
 app.MapControllers();
 
 app.Run();
