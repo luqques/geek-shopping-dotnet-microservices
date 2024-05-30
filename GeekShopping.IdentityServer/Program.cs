b53ec7432ce6f5402d800378a37da482a806bf14
Lucas da Cunha <luques.cunha@gmail.com>
2024-05-29 21:42:04 -0300

    Adicionado Configuration do banco de dados no Program do Identity Server

--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,4 +1,14 @@
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 2)))
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
