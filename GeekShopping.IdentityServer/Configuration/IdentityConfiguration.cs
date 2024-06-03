67060993ced780518ce3ff1b2497b31a3f62fe4b
Lucas da Cunha <luques.cunha@gmail.com>
2024-06-03 20:28:46 -0300

    Adicionando o geek_shopping nos AllowedScopes

--- GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
+++ GeekShopping.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -24,7 +24,6 @@ namespace GeekShopping.IdentityServer.Configuration
                 new ApiScope(name: "read", "Read data."),
                 new ApiScope(name: "write", "Write data."),
                 new ApiScope(name: "delete", "Delete data."),
-                //new ApiScope(name: "identityserverapi", displayName: "manage identityserver api endpoints.")
             };
 
         public static IEnumerable<Client> Clients =>
@@ -33,14 +32,14 @@ namespace GeekShopping.IdentityServer.Configuration
                 new Client
                 {
                     ClientId = "client",
-                    ClientSecrets = { new Secret("my_super_secret".ToSha256())},
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = { "read", "write", "profile" }
                 },
                 new Client
                 {
                     ClientId = "geek_shopping",
-                    ClientSecrets = { new Secret("my_super_secret".ToSha256())},
+                    ClientSecrets = { new Secret("my_super_secret".Sha256())},
                     AllowedGrantTypes = GrantTypes.Code,
                     RedirectUris = {"https://localhost:4430/signin-oidc"},
                     PostLogoutRedirectUris = {"https://localhost:4430/signout-callback-oidc"},
@@ -49,6 +48,7 @@ namespace GeekShopping.IdentityServer.Configuration
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
                         IdentityServerConstants.StandardScopes.Email,
+                        "geek_shopping"
                     }
                 }
             };
